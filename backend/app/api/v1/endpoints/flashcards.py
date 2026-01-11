217c748be9bddb757ce73b7dd220e95b26339eeb
Ryan McUmber <ryanmma.rm@gmail.com>
2026-01-11 13:36:55 -0500

    starting Flashcard service

--- backend/app/api/v1/endpoints/flashcards.py
+++ backend/app/api/v1/endpoints/flashcards.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends
-from dal import flashcard_dal
+from services import flashcard_service
 
 router = APIRouter()
 
 
 @router.get("/{deck_id}")
 async def list_flashcards(deck_id):
-    return flashcard_dal.get(deck_id)
+    return ["party"]
--- backend/app/services/flashcard_service.py
+++ backend/app/services/flashcard_service.py
@@ -1,2 +1,8 @@
-import dal.flashcard_dal
-from db import session
+from typing import List, Optional
+from dal import flashcard_dal as Flashcard_dal
+from models.db import flashcard
+
+
+class FlashcardService:
+    def __init__(self, flashcard_dal: Flashcard_dal):
+        self.flashcard_dal = flashcard_dal
