b6b53cb7e9e4408df190882e5e815cfd9a1aa588
Ryan McUmber <ryanmma.rm@gmail.com>
2026-01-11 14:45:20 -0500

    service layer outline

--- backend/app/services/flashcard_service.py
+++ backend/app/services/flashcard_service.py
@@ -1,8 +1,29 @@
 from typing import List, Optional
 from dal import flashcard_dal as Flashcard_dal
-from models.db import flashcard
+from models.db import flashcard as Flashcard
 
 
 class FlashcardService:
     def __init__(self, flashcard_dal: Flashcard_dal):
         self.flashcard_dal = flashcard_dal
+
+    # add flashcard to a deck
+    def add_flashcard_to_deck(self, flashcard: Flashcard) -> Flashcard:
+        pass
+
+    # use case: when you fell confident you know the answer
+    # but don't want to delete it or see it for a bit
+    def archive_flashcard(self, flashcard: Flashcard) -> Flashcard:
+        pass
+
+    def update_knowledge_strength(self, flashcard_id: int) -> bool:
+        pass
+
+    def remove_flashcard_from_deck(self, flashcard: Flashcard) -> bool:
+        pass
+
+    def flashcards_of_one_knowledge_strength():
+        pass
+
+    def flashcards_of_one_knowledge_strength_from_one_deck():
+        pass
